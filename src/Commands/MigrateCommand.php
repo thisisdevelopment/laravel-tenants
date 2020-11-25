1cb94fb226f243eb97ce1f87d7ea4548555fe353
sarp <sarpderinsu@hotmail.nl>
2020-11-25 14:12:46 +0100

    Php allows extra parameters, removed try-catch logic

--- src/Commands/MigrateCommand.php
+++ src/Commands/MigrateCommand.php
@@ -2,22 +2,10 @@
 
 namespace ThisIsDevelopment\LaravelTenants\Commands;
 
-use ArgumentCountError;
-use Illuminate\Contracts\Events\Dispatcher;
 use Illuminate\Database\Console\Migrations\MigrateCommand as BaseMigrateCommand;
-use Illuminate\Database\Migrations\Migrator;
 use ThisIsDevelopment\LaravelTenants\Traits\SupportsTenantMigrations;
 
 class MigrateCommand extends BaseMigrateCommand
 {
     use SupportsTenantMigrations;
-
-    public function __construct(Migrator $migrator, Dispatcher $dispatcher)
-    {
-        try {
-            parent::__construct($migrator, $dispatcher);
-        } catch (ArgumentCountError $e) {
-            parent::__construct($migrator);
-        }
-    }
 }
